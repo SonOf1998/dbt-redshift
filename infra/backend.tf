67ce03e654fee9113e81f5e3a0b9f5bcc01899b8
Mate Kis <40013370+SonOf1998@users.noreply.github.com>
2023-06-18 08:16:09 +0200

    combined workflow file & corrected backend syntax for tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,8 @@
 terraform {
+  # backend does not allow variables
   backend "s3" {
-    bucket = "terraform-states-mkis-${var.region}"
+    bucket = "terraform-states-mkis-eu-central-1"
     key    = "dbt-redshift"
-    region = var.region
+    region = "eu-central-1"
   }
 }
